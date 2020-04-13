a398ef89e3c124445be716281cac77afa6b69d20
Pugal M <Pugal M@Pugal>
2020-04-13 08:24:52 +0530

    Testing

---
+++ src/testingProject/Sampleproject.java
@@ -0,0 +1,14 @@
+package testingProject;
+
+public class Sampleproject {
+	
+	
+	public static void main(String[]args){
+
+		System.out.println("Ananth");
+
+		System.out.println("Sanjay");
+
+		}
+
+}
